9dbfa67e4668b6c6c325e287292d5d491b49b690
Anh.PhamPhu <anhpham.96.k@gmail.com>
2022-07-10 18:52:18 +0900

    Add layout and routes

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -2,12 +2,27 @@ import { createRouter, createWebHistory } from "vue-router";
 import Dashboard from '../views/Dashboard.vue';
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
+import Surveys from '../views/Surveys.vue';
+import DefaultLayout from '../components/DefaultLayout.vue';
 
 const routes = [
     {
         path: '/',
+        redirect: '/dashboard', 
         name: 'Dashboard',
-        component: Dashboard
+        component: DefaultLayout,
+        children: [
+            {
+                path: '/dashboard',
+                name: 'Dashboard',
+                component: Dashboard
+            },
+            {
+                path: '/surveys',
+                name: 'Surveys',
+                component: Surveys
+            }
+        ]
     },
     {
         path: '/login',
--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -1,7 +1,8 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-    <header class="bg-white shadow">
+  <header class="bg-white shadow">
       <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold text-gray-900">Dashboard</h1>
+        <h1 class="text-3xl font-bold text-gray-900">Surveys</h1>
       </div>
     </header>
     <main>
@@ -15,13 +16,6 @@
     </main>
 </template>
 
-<script>
-export default {
-    name: "Dashboard",
-}
+<script setup>
 
 </script>
-
-<style scoped>
-
-</style>
