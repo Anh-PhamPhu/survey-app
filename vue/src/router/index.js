21667d181a304ea2cea932588d5f50d711f58dcb
Anh.PhamPhu <anhpham.96.k@gmail.com>
2022-07-10 20:56:24 +0900

    Created logic for auths pages

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -4,12 +4,16 @@ import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
 import Surveys from '../views/Surveys.vue';
 import DefaultLayout from '../components/DefaultLayout.vue';
+import AuthLayout from '../components/AuthLayout.vue';
+import store from '../store';
+
 
 const routes = [
     {
         path: '/',
         redirect: '/dashboard', 
         name: 'Dashboard',
+        meta: {requireAuth: true},
         component: DefaultLayout,
         children: [
             {
@@ -25,15 +29,25 @@ const routes = [
         ]
     },
     {
-        path: '/login',
-        name: 'Login',
-        component: Login
+        path: '/auth',
+        redirect: '/login',
+        name: 'Auth',
+        meta: {isGuest: true},
+        component: AuthLayout,
+        children: [
+            {
+                path: '/login',
+                name: 'Login',
+                component: Login
+            },
+            {
+                path: '/register',
+                name: 'Register',
+                component: Register
+            },
+        ],
     },
-    {
-        path: '/register',
-        name: 'Register',
-        component: Register
-    },
+    
 
 ];
 
@@ -41,5 +55,15 @@ const router = createRouter({
     history: createWebHistory(),
     routes,
 })
+
+router.beforeEach((to, from, next) => {
+    if(to.meta.requireAuth && !store.state.user.token){
+        next({name: 'Login'});
+    }else if(store.state.user.token && (to.meta.isGuest)) {
+        next({name : 'Dashboard'});
+    } else {
+        next();
+    }
+});
 
 export default router;
