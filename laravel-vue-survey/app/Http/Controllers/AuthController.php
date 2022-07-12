b29c597067fed6c9902ea61163912e39f8cd7204
Anh.PhamPhu <anhpham.96.k@gmail.com>
2022-07-13 00:31:30 +0900

    Backend: add login logic. Frontend: add login and register using axios, and create errorMsg in both page

--- laravel-vue-survey/app/Http/Controllers/AuthController.php
+++ laravel-vue-survey/app/Http/Controllers/AuthController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Http\User;
 use Illuminate\Validation\Rules\Password;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -12,20 +13,46 @@ class AuthController extends Controller
     public function register(Request $request){
         $data = $request->validate([
             'name' => 'required|string',
-            'email' => 'required|email|string|unique:users,mail',
+            'email' => 'required|email|string|unique:users,email',
             'password' => [
                 'required',
-                'confirmed',
                 Password::min(8)->mixedCase()->numbers()->symbols()
             ]
         ]);
 
         /** @var \App\Models\User $user */
         $user = User::create([
-            'name ' => $data['name'],
-            'email ' => $data['email'],
-            'password ' => bcrypt($data['name']),
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['name']),
         ]);
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ], 200);
+    }
+
+    public function login(Request $request){
+        $credentials = $request->validate([
+            'email' => 'required|email|string|exists:users,email',
+            'password' => [
+                'required',
+            ],
+            'remember' => 'boolean'
+        ]);
+
+        $remember = $credentials['remember'] ?? false;
+        unset($credentials['remember']);
+
+        if(!Auth::attempt($credentials, $remember)){
+            return response([
+                'error' => 'The Provided credentials are not correct'
+            ], 422);
+        }
+
+        $user = Auth::user();
         $token = $user->createToken('main')->plainTextToken;
 
         return response([
--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -9,7 +9,14 @@
         </p>
       </div>
       <form class="mt-8 space-y-6" @submit="register">
-        <input type="hidden" name="remember" value="true" />
+        <div v-if="errorMsg" class="flex items-center justify-between py-3 px-5 bg-red-500 text-white rounded">
+          {{errorMsg}}
+          <span @click="errorMsg = ''" class="w-8 h-8 flex items-center justify-center rounded-full transition-colors cursor-pointer hover:bg-[rgba(0,0,0,0.2)]">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </span>
+        </div>
         <div class="rounded-md shadow-sm -space-y-px">
             <div>
                 <label for="fullname" class="sr-only">Full name</label>
@@ -24,14 +31,6 @@
                 <input v-model="user.password" id="password" name="password" type="password" autocomplete="current-password" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password" />
             </div>
         </div>
-
-        <div class="flex items-center justify-between">
-          <div class="flex items-center">
-            <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" />
-            <label for="remember-me" class="ml-2 block text-sm text-gray-900"> Remember me </label>
-          </div>
-        </div>
-
         <div>
           <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
             <span class="absolute left-0 inset-y-0 flex items-center pl-3">
@@ -47,8 +46,10 @@
 import { LockClosedIcon } from '@heroicons/vue/solid'
 import {useRouter} from 'vue-router';
 import store from '../store';
+import {ref} from 'vue';
 
 const router = useRouter();
+let errorMsg = ref('');
 
 const user = {
   name: '',
@@ -60,10 +61,13 @@ function register(ev){
   ev.preventDefault();
   store
     .dispatch('register', user)
-    .then((res) => {
-      router.push({
-        name: 'Dashboard',
-      });
-    })
+      .then((res) => {
+        router.push({
+          name: 'Dashboard',
+        });
+      })
+      .catch((err) => {
+        errorMsg.value = err.response.data.message;
+      })
 }
 </script>
