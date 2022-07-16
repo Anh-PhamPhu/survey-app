d76499d070511647c05f943dd825c2cc8294bb1b
Anh.PhamPhu <anhpham.96.k@gmail.com>
2022-07-16 16:29:05 +0900

    BackEnd: Created CRUD for the survey

--- laravel-vue-survey/app/Models/Survey.php
+++ laravel-vue-survey/app/Models/Survey.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Spatie\Luggable\HasSlug;
+use Spatie\Sluggabble\SlugOptions;
 class Survey extends Model
 {
-    use HasFactory;
+    use HasFactory, HasSlug;
+
+    protected $fillable = ['user_id', 'title', 'slug', 'status', 'description', 'exprire_date'];
+
+    public function getSlugOption(): SlugOptions{
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
 }
