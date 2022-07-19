9c4761714493f0f777c49a2808f07da6ed38e4f9
Anh.PhamPhu <103031659+Anh-PhamPhu@users.noreply.github.com>
2022-07-19 18:41:19 +0900

    Update axios.js

--- axios.js
+++ axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import store from './src/store';
 
-const baseURL = 'http://localhost:8000/api';
+const baseURL = process.env.HTTP_URL || 'https://ppa-survey-app-api.herokuapp.com/api';
 
 const axiosClient = axios.create({
     baseURL
